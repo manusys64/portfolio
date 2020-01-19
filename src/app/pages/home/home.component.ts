1bf8d02a33607a05c7691851059399af11818464
Fukusui <lavalla@protonmail.com>
2020-01-18 20:01:02 -0500

    added home page background

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.styl']
 })
-export class HomeComponent implements OnInit {
-
+export class HomeComponent {
+  chain = "https://i.giphy.com/media/QUXK4fFx8kYci4mcar/giphy.webp"
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
