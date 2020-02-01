720ba9faf71a75c9a29705164605105e3aaa31d2
Fukusui <lavalla@protonmail.com>
2020-02-01 14:31:59 -0500

    AWS S3 setup

--- server/server/settings.py
+++ server/server/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'markdownx',
     'psycopg2',
+    'storages',
     'rest_framework',
     'contacts',
     'infos'
@@ -113,16 +114,48 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET")
+AWS_STORAGE_BUCKET_NAME = 'ryu-static'
+# AWS_FILE_EXPIRE = 200
+# AWS_PRELOAD_METADATA = True
+# AWS_QUERYSTRING_AUTH = True
+#
+DEFAULT_FILE_STORAGE = 'server.utils.MediaRootS3BotoStorage'
+STATICFILES_STORAGE = 'server.utils.StaticRootS3BotoStorage'
+#
+# S3DIRECT_REGION = 'us-west-2'
+S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_ROOT = MEDIA_URL
+STATIC_URL = S3_URL + 'static/'
+# AWS_PRELOAD_METADATA = True
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+#
+# two_months = datetime.timedelta(days=61)
+# date_two_months_later = datetime.date.today() + two_months
+# expires = date_two_months_later.strftime("%A, %d %B %Y 20:00:00 GMT")
+# AWS_S3_FILE_OVERWRITE = True
+# AWS_HEADERS = {
+#   'Expires': expires,
+#     'Cache-Control': 'max-age=%d' % (int(two_months.total_seconds()), ),
+#     }
 
 #REST_Framework
 REST_FRAMEWORK = {
