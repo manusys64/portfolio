b7889e863ba08dd97d646fa5c3ef6cd12cd2b04f
Fukusui <lavalla@protonmail.com>
2020-02-03 18:35:34 -0500

    added meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { mergeMap, filter, map, tap } from 'rxjs/operators';
+import { Meta } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
@@ -10,10 +12,12 @@ import { mergeMap, filter, map, tap } from 'rxjs/operators';
 })
 export class AppComponent implements OnInit{
   constructor(private router: Router,
+    private metaTagService: Meta,
     private activatedRoute: ActivatedRoute,
     private titleService: Title) {}
 
   ngOnInit() {
+    //Title Service//
     this.router.events.pipe(
       filter((event) => event instanceof NavigationEnd),
       map(() => this.activatedRoute),
@@ -23,7 +27,17 @@ export class AppComponent implements OnInit{
       }),
       filter((route) => route.outlet ==='primary'),
       mergeMap((route) => route.data)
-    ).subscribe((event) => this.titleService.setTitle(event['title']))
+    ).subscribe((event) => this.titleService.setTitle(event['title']));
+    //SEO Tags
+		this.metaTagService.addTags([
+		  { name: 'keywords', content: 'Full Stack Developer, Web Development, Blockchain' },
+      { name: 'robots', content: 'index, follow' },
+      { name: 'author', content: 'Ryusuke Lavalla' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'date', content: '2020-2-1', scheme: 'YYYY-MM-DD' },
+      { charset: 'UTF-8' },
+      { name: 'description', content: 'Ryusuke is a full stack web developer, Blockchain developer, Full stack application developer, based in Brooklyn New York.'}
+		]);
   }
 
 }
