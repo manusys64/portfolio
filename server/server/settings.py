32dc52772b5c47fbd2ff03acf794e0ba68adb7b9
Ubuntu <ubuntu@ip-172-31-59-96.ec2.internal>
2020-02-03 17:12:54 +0000

    updated allowed host in settings.py

--- server/server/settings.py
+++ server/server/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['localhost', '34.199.187.54']
+ALLOWED_HOSTS = ['localhost', '34.199.187.54', 'www.ryusuke.live', 'ryusuke.live']
 
 # Application definition
 
